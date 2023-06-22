d8c5e0eca57aa09c72b1b788a93ab5e0d463691a
Matt Hunzinger <matthunz2@gmail.com>
2023-06-21 21:57:48 -0400

    Create new View trait

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+pub struct Context {}
+
+pub trait View {
+    fn view(&mut self, cx: &mut Context);
+}
+
+impl View for String {
+    fn view(&mut self, cx: &mut Context) {
+        dbg!(self);
+    }
+}
