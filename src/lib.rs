680d5240cee9494d174a2b5bc1d3ac8e88420b9f
Matt Hunzinger <matthunz2@gmail.com>
2023-06-22 14:46:02 -0400

    Create new Semantics trait with build and rebuild methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,140 +1,31 @@
-use std::{collections::HashMap, num::NonZeroU128};
-
-pub mod view;
-use accesskit::{NodeBuilder, NodeClassSet, NodeId, Role, TreeUpdate};
-use slotmap::{DefaultKey, SlotMap};
-use taffy::{
-    prelude::{Node, Size},
-    style::{FlexDirection, Style},
-    style_helpers::{points, TaffyMaxContent},
-    Taffy,
-};
-pub use view::View;
-
-pub struct SemanticsContext<'a> {
-    next_id: &'a mut NonZeroU128,
-    unused_ids: &'a mut Vec<NodeId>,
-    tree_update: &'a mut TreeUpdate,
-    node_children: &'a mut Vec<NodeId>,
-}
-
-impl SemanticsContext<'_> {
-    pub fn node_id(&mut self) -> NodeId {
-        if let Some(id) = self.unused_ids.pop() {
-            id
-        } else {
-            let id = *self.next_id;
-            *self.next_id = self.next_id.checked_add(1).unwrap();
-            NodeId(id)
-        }
-    }
-}
+use accesskit::{Node, NodeBuilder, NodeClassSet, Role};
 
 pub struct Context {
-    next_id: NonZeroU128,
-    unused_ids: Vec<NodeId>,
-    handlers: HashMap<usize, Box<dyn FnMut()>>,
-    taffy: Taffy,
-    root: Option<DefaultKey>,
-    children: Vec<Node>,
-    widgets: SlotMap<DefaultKey, Box<dyn Widget>>,
-    updated: Vec<DefaultKey>,
-    is_first_tree_update: bool,
+    nodes: Vec<Node>,
 }
 
-impl Context {
-    pub fn new() -> Self {
-        Self {
-            next_id: unsafe { NonZeroU128::new_unchecked(2) },
-            unused_ids: Vec::new(),
-            handlers: HashMap::new(),
-            taffy: Taffy::new(),
-            root: None,
-            children: Vec::new(),
-            widgets: SlotMap::new(),
-            updated: Vec::new(),
-            is_first_tree_update: true,
+pub trait Semantics {
+    fn build(&mut self, cx: &mut Context);
+
+    fn rebuild(&mut self, cx: &mut Context, old: Self);
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Text {
+    string: String,
+}
+
+impl Semantics for Text {
+    fn build(&mut self, cx: &mut Context) {
+        let mut builder = NodeBuilder::new(Role::StaticText);
+        builder.set_value(self.string.clone());
+        let node = builder.build(&mut NodeClassSet::lock_global());
+        cx.nodes.push(node);
+    }
+
+    fn rebuild(&mut self, cx: &mut Context, old: Self) {
+        if *self != old {
+            self.build(cx)
         }
-    }
-
-    pub fn handle(&mut self, id: usize) {
-        self.handlers.get_mut(&id).unwrap()();
-    }
-
-    pub fn layout(&mut self) {
-        let root = if let Some(root) = self.root {
-            self.taffy.set_children(root, &self.children).unwrap();
-            root
-        } else {
-            let style = Style {
-                flex_direction: FlexDirection::Column,
-                size: Size {
-                    width: points(800.0),
-                    height: points(600.0),
-                },
-                ..Default::default()
-            };
-            let root = self.taffy.new_with_children(style, &self.children).unwrap();
-            self.root = Some(root);
-            root
-        };
-
-        self.taffy.compute_layout(root, Size::MAX_CONTENT).unwrap();
-    }
-
-    pub fn tree_update(&mut self) -> TreeUpdate {
-        let mut tree_update = TreeUpdate::default();
-
-        if self.is_first_tree_update {
-            let mut node_children = Vec::new();
-
-            for id in std::mem::take(&mut self.updated) {
-                let widget = self.widgets.get_mut(id).unwrap();
-                widget.semantics(&mut SemanticsContext {
-                    next_id: &mut self.next_id,
-                    unused_ids: &mut self.unused_ids,
-                    tree_update: &mut tree_update,
-                    node_children: &mut node_children,
-                });
-            }
-
-            const WINDOW_ID: NodeId = NodeId(unsafe { NonZeroU128::new_unchecked(1) });
-            let mut builder = NodeBuilder::new(Role::Window);
-            builder.set_children(node_children);
-            builder.set_name("WINDOW_TITLE");
-            let node = builder.build(&mut NodeClassSet::lock_global());
-            tree_update.nodes.push((WINDOW_ID, node));
-
-            self.is_first_tree_update = false;
-        }
-
-        tree_update
-    }
-}
-
-pub trait Widget {
-    fn semantics(&mut self, cx: &mut SemanticsContext);
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::{view::Text, Context, View};
-    use accesskit::Role;
-
-    #[test]
-    fn it_creates_tree_updates() {
-        let mut cx = Context::new();
-        let mut text = Text::new("Test");
-        text.view(&mut cx);
-
-        cx.layout();
-
-        let tree_update = cx.tree_update();
-        assert_eq!(tree_update.nodes.len(), 2);
-        assert_eq!(tree_update.nodes[0].1.role(), Role::StaticText);
-        assert_eq!(tree_update.nodes[1].1.role(), Role::Window);
-
-        let tree_update = cx.tree_update();
-        assert_eq!(tree_update.nodes.len(), 0);
     }
 }
