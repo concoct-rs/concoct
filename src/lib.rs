a0a68f015788a652dfc6a78113f94701e3068bde
Matt Hunzinger <matthunz2@gmail.com>
2023-07-01 04:29:09 -0400

    Update docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,16 @@
 //! Generic UI compiler and runtime library.
+//!
+//! This crate provides positional memoization where programs are defined as a composition of [`composable`] functions.
+//! The results of these functions are cached based on the position of the function call.
+//! For example:
+//! ```no_run
+//! #[composable]
+//! fn f() -> i32
+//! 
+//! // will become:
+//! 
+//! fn f() -> impl Composable<Output = i32>
+//! ```
 
 use std::{
     any::TypeId,
@@ -16,6 +28,9 @@ pub use composer::Composer;
 mod slot_table;
 use slot_table::Slot;
 
+/// Composer is the interface that is targeted by the [`composable`] macro and used by code generation helpers.
+/// It is highly recommended that direct calls these be avoided as the runtime assumes that the calls are generated
+/// by the compiler and contain only a minimum amount of state validation.
 pub trait Compose {
     fn start_restart_group(&mut self, type_id: TypeId);
 
@@ -29,6 +44,11 @@ pub trait Compose {
 
     fn skip_to_group_end(&mut self);
 
+    /// Cache a value in the composition.
+    /// During initial composition `f` is called to produce the value that is then stored in the slot table.
+    /// During recomposition, if `is_invalid` is false the value is obtained from the slot table and `f` is not invoked.
+    /// If `is_invalid` is false a new value is produced by calling [block]
+    /// and the slot table is updated to contain the new value.
     fn cache<T>(&mut self, is_invalid: bool, f: impl FnOnce() -> T) -> T
     where
         T: Clone + Hash + PartialEq + 'static;
@@ -53,6 +73,8 @@ macro_rules! current_composer {
     () => {};
 }
 
+/// Remember the value produced by `f`. `f` will only be evaluated during the composition.
+/// Recomposition will always return the value produced by composition.
 #[composable]
 pub fn remember<T, F>(f: F) -> T
 where
