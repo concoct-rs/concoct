cf735c0ce2340ccae7734355d8f002685588c97a
Matt Hunzinger <matthunz2@gmail.com>
2023-06-30 23:11:12 -0400

    Test slot table insert

--- tests/slot_table.rs
+++ tests/slot_table.rs
@@ -3,17 +3,26 @@ use std::any::TypeId;
 
 #[test]
 fn it_is_empty() {
-    let table = SlotTable::default();
-    assert!(table.is_empty());
+    let slots = SlotTable::default();
+    assert!(slots.is_empty());
 
-    let table = table.write(|writer| {
+    let slots = slots.write(|writer| {
         writer.begin_insert();
-
         writer.start_group(TypeId::of::<()>(), None);
         writer.end_group();
-
         writer.end_insert();
     });
+    assert!(!slots.is_empty());
+}
 
-    assert!(!table.is_empty());
+#[test]
+fn it_can_insert() {
+    let slots = SlotTable::default();
+    slots.write(|writer| {
+        writer.begin_insert();
+        writer.start_group(TypeId::of::<()>(), None);
+        writer.update(Some(Box::new(1)));
+        writer.end_group();
+        writer.end_insert();
+    });
 }
