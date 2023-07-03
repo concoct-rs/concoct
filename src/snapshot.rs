c7d8f7030f077e5e5f70304c2b7d43cfc7c17141
Matt Hunzinger <matthunz2@gmail.com>
2023-07-03 02:10:22 -0400

    Track reads and writes in state

--- src/snapshot.rs
+++ src/snapshot.rs
@@ -1,10 +1,38 @@
 use std::{
     any::Any,
+    cell::RefCell,
+    collections::HashSet,
     marker::PhantomData,
+    mem,
     ops::Deref,
     sync::{Arc, Mutex, MutexGuard, RwLock},
 };
 use tokio::sync::mpsc::{self, UnboundedReceiver, UnboundedSender};
+
+#[derive(Debug, Default)]
+pub struct Scope {
+    reads: HashSet<u64>,
+    writes: HashSet<u64>,
+}
+
+impl Scope {
+    pub fn enter(self, f: impl FnOnce()) -> Self {
+        let parent = LOCAL_SCOPE
+            .try_with(|scope| mem::replace(&mut *scope.borrow_mut(), Some(self)))
+            .unwrap();
+
+        f();
+
+        LOCAL_SCOPE
+            .try_with(|scope| mem::replace(&mut *scope.borrow_mut(), parent))
+            .unwrap()
+            .unwrap()
+    }
+}
+
+thread_local! {
+    static LOCAL_SCOPE: RefCell<Option<Scope>> = RefCell::new(None);
+}
 
 struct Operation {
     value: Arc<Mutex<Box<dyn Any + Send + Sync>>>,
@@ -26,19 +54,37 @@ impl Snapshot {
 
     pub fn apply(&mut self) {
         while let Ok(mut next) = self.rx.try_recv() {
-            let mut value = next.value.lock().unwrap();
+            let mut guard = next.value.lock().unwrap();
+            let value: &mut dyn Any = guard.as_mut();
             (next.f)(&mut *value)
         }
     }
 }
 
 pub struct State<T> {
+    id: u64,
     value: Arc<Mutex<Box<dyn Any + Send + Sync>>>,
     _marker: PhantomData<T>,
 }
 
 impl<T> State<T> {
+    pub fn new(value: T) -> Self
+    where
+        T: Send + Sync + 'static,
+    {
+        Self {
+            id: 0,
+            value: Arc::new(Mutex::new(Box::new(value))),
+            _marker: PhantomData,
+        }
+    }
     pub fn get(&self) -> Guard<T> {
+        LOCAL_SCOPE
+            .try_with(|scope| {
+                scope.borrow_mut().as_mut().unwrap().reads.insert(self.id);
+            })
+            .unwrap();
+
         let mutex = self.value.lock().unwrap();
         Guard {
             mutex,
@@ -50,13 +96,19 @@ impl<T> State<T> {
     where
         T: Send + Sync + 'static,
     {
+        LOCAL_SCOPE
+            .try_with(|scope| {
+                scope.borrow_mut().as_mut().unwrap().writes.insert(self.id);
+            })
+            .unwrap();
+
         TX.read()
             .unwrap()
             .as_ref()
             .unwrap()
             .send(Operation {
                 value: self.value.clone(),
-                f: Box::new(move |any| f(&mut *any.downcast_mut().unwrap())),
+                f: Box::new(move |any| f(any.downcast_mut().unwrap())),
             })
             .unwrap();
     }
@@ -72,5 +124,30 @@ impl<'a, T: 'static> Deref for Guard<'a, T> {
 
     fn deref(&self) -> &Self::Target {
         self.mutex.downcast_ref().unwrap()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{snapshot::Snapshot, State};
+    use super::Scope;
+
+    #[test]
+    fn it_works() {
+        let mut snapshot = Snapshot::take();
+
+        Scope::default().enter(|| {
+            let state = State::new(0);
+
+            let scope = Scope::default().enter(|| {
+                assert_eq!(*state.get(), 0);
+
+                state.update(|count| *count += 1);
+            });
+            dbg!(scope);
+
+            snapshot.apply();
+            assert_eq!(*state.get(), 1);
+        });
     }
 }
