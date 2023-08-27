e35093f0b68520f71ffc295abdf8c2b47a8f57f3
Matt Hunzinger <matthunz2@gmail.com>
2023-08-27 08:08:55 -0400

    Impl editing in todomvc

--- examples/todomvc/src/main.rs
+++ examples/todomvc/src/main.rs
@@ -1,8 +1,7 @@
 use concoct::attr::{attr, class, event_key_code, event_target_value, on, value};
-use concoct::view::html::{
-    button, div, footer, h1, header, input, label, li, p, section, span, ul,
-};
+use concoct::view::html::{button, div, footer, h1, header, input, label, li, p, section, ul};
 use concoct::view::{lazy, View};
+use concoct::Attribute;
 use std::mem;
 
 enum Event {
@@ -10,6 +9,17 @@ enum Event {
     UpdateInput(String),
     Add,
     Remove(u32),
+    Check(u32),
+    Edit { id: u32, is_editing: bool },
+    Update { id: u32, content: String },
+}
+
+#[derive(Clone, PartialEq)]
+struct Todo {
+    id: u32,
+    content: String,
+    is_editing: bool,
+    is_completed: bool,
 }
 
 #[derive(Default)]
@@ -17,7 +27,7 @@ struct State {
     input: String,
     next_id: u32,
     unused_ids: Vec<u32>,
-    todos: Vec<(u32, String)>,
+    todos: Vec<Todo>,
 }
 
 fn view(state: &State) -> impl View<Event> {
@@ -43,13 +53,7 @@ fn view_input(state: &State) -> impl View<Event> {
                 event.prevent_default();
                 Event::UpdateInput(event_target_value(&event))
             }),
-            on("keydown", |event| {
-                if event_key_code(&event) == 13 {
-                    Event::Add
-                } else {
-                    Event::None
-                }
-            }),
+            on_enter(|| Event::Add),
         )),
     ))
 }
@@ -59,22 +63,78 @@ fn view_entries(state: &State) -> impl View<Event> {
         state
             .todos
             .iter()
-            .map(|(id, content)| (*id, view_entry(*id, content.clone())))
+            .map(|todo| (todo.id, view_entry(&todo)))
             .collect::<Vec<_>>(),
     )
 }
 
-fn view_entry(id: u32, content: String) -> impl View<Event> {
-    li().then(div().modify(class("view")).then((
-        label().then(content),
-        button().modify((class("destroy"), on("click", move |_| Event::Remove(id)))),
-    )))
+fn view_entry(todo: &Todo) -> impl View<Event> {
+    let id = todo.id;
+    let class_list = if todo.is_completed {
+        if todo.is_editing {
+            "completed editing"
+        } else {
+            "completed"
+        }
+    } else if todo.is_editing {
+        "editing"
+    } else {
+        ""
+    };
+
+    li().modify(class(class_list)).then((
+        div().modify(class("view")).then((
+            input().modify((
+                class("toggle"),
+                attr("type", "checkbox"),
+                attr("checked", todo.is_completed.to_string()),
+                on("click", move |_| Event::Check(id)),
+            )),
+            label()
+                .modify(on("click", move |_| Event::Edit {
+                    id,
+                    is_editing: true,
+                }))
+                .then(todo.content.clone()),
+            button().modify((class("destroy"), on("click", move |_| Event::Remove(id)))),
+        )),
+        input().modify((
+            class("edit"),
+            value(todo.content.clone()),
+            attr("name", "content"),
+            on("input", move |event| {
+                event.prevent_default();
+                Event::Update {
+                    id,
+                    content: (event_target_value(&event)),
+                }
+            }),
+            on("blur", move |_| Event::Edit {
+                id,
+                is_editing: false,
+            }),
+            on_enter(move || Event::Edit {
+                id,
+                is_editing: false,
+            }),
+        )),
+    ))
 }
 
 fn view_footer() -> impl View<Event> {
     footer()
         .modify(class("info"))
-        .then(p().then("Double-click to edit a todo"))
+        .then(p().then("Click to edit a todo"))
+}
+
+fn on_enter(f: impl Fn() -> Event + 'static) -> impl Attribute<Event> {
+    on("keydown", move |event| {
+        if event_key_code(&event) == 13 {
+            f()
+        } else {
+            Event::None
+        }
+    })
 }
 
 fn main() {
@@ -86,16 +146,36 @@ fn main() {
                 state.input = value;
             }
             Event::Add => {
-                let title = mem::take(&mut state.input);
+                let content = mem::take(&mut state.input);
                 let id = state.unused_ids.pop().unwrap_or_else(|| {
                     let id = state.next_id;
                     state.next_id += 1;
                     id
                 });
-                state.todos.push((id, title));
+                state.todos.push(Todo {
+                    id,
+                    content,
+                    is_editing: false,
+                    is_completed: false,
+                });
+            }
+            Event::Check(id) => {
+                if let Some(todo) = state.todos.iter_mut().find(|todo| todo.id == id) {
+                    todo.is_completed = !todo.is_completed;
+                }
+            }
+            Event::Edit { id, is_editing } => {
+                if let Some(todo) = state.todos.iter_mut().find(|todo| todo.id == id) {
+                    todo.is_editing = is_editing;
+                }
+            }
+            Event::Update { id, content } => {
+                if let Some(todo) = state.todos.iter_mut().find(|todo| todo.id == id) {
+                    todo.content = content;
+                }
             }
             Event::Remove(id) => {
-                if let Some(idx) = state.todos.iter().position(|(k, _)| id == *k) {
+                if let Some(idx) = state.todos.iter().position(|todo| todo.id == id) {
                     state.todos.remove(idx);
                     state.unused_ids.push(id);
                 }
