ad63a842f7f918f7ac2f87ac17b8026b0895252f
Matt Hunzinger <matthunz2@gmail.com>
2023-08-28 03:36:31 -0400

    Clean up and add docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,16 @@
 #![cfg_attr(docsrs, feature(doc_cfg))]
 
-pub mod modify;
+//! A cross-platform framework for efficient user interfaces.
+//!
+//! Concoct is statically-typed UI library for building applications with Rust
+//! that run on multiple platforms.
+
+mod modify;
 pub use modify::Modify;
 
 pub mod view;
+pub use view::View;
 
 #[cfg(feature = "web")]
+#[cfg_attr(docsrs, doc(cfg(feature = "web")))]
 pub mod web;
