e4d243657b722d62629636aa67f316de6ae68bb4
Matt Hunzinger <matthunz2@gmail.com>
2023-11-07 06:47:12 -0500

    Restart with new ideas

--- src/native/element.rs
+++
@@ -1,5 +0,0 @@
-use taffy::Taffy;
-
-pub trait Element {
-    fn paint(&mut self, taffy: &Taffy, canvas: &mut skia_safe::Canvas);
-}
--- src/native/view/mod.rs
+++
@@ -1,5 +0,0 @@
-mod canvas;
-pub use canvas::{canvas, Canvas};
-
-mod text;
-pub use text::text;
