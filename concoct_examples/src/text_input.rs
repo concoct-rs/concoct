a86cef4470100be292d31f23da0b7c242a996007
Matt Hunzinger <matthunz2@gmail.com>
2023-11-08 04:58:04 -0500

    Rename examples dir

---
+++ concoct_examples/src/text_input.rs
@@ -0,0 +1,12 @@
+use concoct::{use_signal, Html, View};
+
+pub fn app() -> impl View {
+    let label = use_signal(|| String::new());
+    
+    Html::input()
+        .attr("value", label.read().clone())
+        .on_input(move |event| {
+            event.prevent_default();
+            *label.write() = event.target().unwrap().value();
+        })
+}
