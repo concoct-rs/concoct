338833421af7931ff08376cc99206cf635ac3fef
Matt Hunzinger <matthunz2@gmail.com>
2023-11-24 21:52:51 -0500

    Fix CI

--- tests/composition.rs
+++ tests/composition.rs
@@ -1,17 +1,6 @@
 use concoct::{Composable, Composition};
 
 #[test]
-fn it_reuses_scopes_for_tuples() {
-    fn app() -> impl Composable {
-        ((), ())
-    }
-
-    let mut composition = Composition::new(app);
-    composition.build();
-    assert_eq!(composition.len(), 1);
-}
-
-#[test]
 fn it_creates_new_scopes_for_multiple_composables() {
     #[derive(Clone, PartialEq)]
     struct Component;
