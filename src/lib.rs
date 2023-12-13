9d48644a75a3411b321b045f8fbfdbab1d090fea
Matt Hunzinger <matthunz2@gmail.com>
2023-12-13 04:13:33 -0500

    Add docs to crate root

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,53 @@
 //! # Concoct
 //!
+//! Concoct is a runtime for user-interfaces in Rust.
+//!
 //! ## Feature flags
-//! Concoct uses a set of feature flags to provide support for `#![no_std]``
+//! Concoct uses a set of feature flags to provide support for `#![no_std]`
 //! (and to reduce the amount of compiled code).
 //!
 //!  - `full`: Enables all features listed below.
 //!  - `rt`: Enables the `Runtime`.
 //!  - `futures`: Enables interop with the `futures` crate (and provides the default `Runtime`).
+//!
+//! ```rust
+//! #[derive(Default)]
+//! pub struct Counter {
+//!     value: i32,
+//! }
+//!
+//! impl Object for Counter {}
+//!
+//! impl Signal<i32> for Counter {}
+//!
+//! impl Slot<i32> for Counter {
+//!     fn handle(&mut self, cx: Handle<Self>, msg: i32) {
+//!         if self.value != msg {
+//!             self.value = msg;
+//!             cx.emit(msg);
+//!         }
+//!     }
+//! }
+//!
+//! #[tokio::main]
+//! async fn main() {
+//!     let rt = Runtime::default();
+//!     let _guard = rt.enter();
+//!
+//!     let a = Counter::default().spawn();
+//!     let b = Counter::default().spawn();
+//!
+//!     a.bind(&b);
+//!
+//!     a.send(1);
+//!     a.send(2);
+//!
+//!     rt.run().await;
+//!
+//!     assert_eq!(a.borrow().value, 2);
+//!     assert_eq!(b.borrow().value, 2);
+//! }
+//! ```
 //!
 
 #![cfg_attr(not(feature = "std"), no_std)]
