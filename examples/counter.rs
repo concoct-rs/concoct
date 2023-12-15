304604c8c69419be3f63656cd03bec1d4223be62
Matt Hunzinger <matthunz2@gmail.com>
2023-12-15 02:03:51 -0500

    Create Context struct

--- examples/counter.rs
+++ examples/counter.rs
@@ -1,3 +1,28 @@
+use concoct::{Context, Handle};
+
+#[derive(Default)]
+struct Counter {
+    value: i32,
+}
+
+impl Counter {
+    fn set_value(cx: &mut Context<Self>, value: i32) {
+        dbg!(value);
+        if cx.value != value {
+            cx.value = value;
+            cx.emit(value);
+        }
+    }
+}
+
 fn main() {
-    
+    let a = Handle::new(Counter::default());
+    let b = Handle::new(Counter::default());
+
+    a.bind(&b, Counter::set_value);
+
+    Counter::set_value(&mut a.cx(), 2);
+
+    assert_eq!(a.borrow().value, 2);
+    assert_eq!(b.borrow().value, 2);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,13 @@ use std::{
     any::{Any, TypeId},
     cell::{Ref, RefCell, RefMut},
     marker::PhantomData,
+    ops::{Deref, DerefMut},
     rc::Rc,
 };
 
 struct ListenerData {
-    type_id: TypeId,
+    msg_type_id: TypeId,
+    listener_type_id: TypeId,
     f: Box<dyn FnMut(&dyn Any)>,
 }
 
@@ -15,15 +17,78 @@ struct Node {
     listeners: Vec<ListenerData>,
 }
 
+pub struct Context<'a, O> {
+    handle: Handle<O>,
+    node: RefMut<'a, Node>,
+}
+
+impl<'a, O> Context<'a, O> {
+    pub fn emit<M: 'static>(&mut self, msg: M) {
+        for listener in &mut self.node.listeners {
+            if listener.msg_type_id == msg.type_id() {
+                (listener.f)(&msg)
+            }
+        }
+    }
+}
+
+
+
+impl<O: 'static> Deref for Context<'_, O> {
+    type Target = O;
+
+    fn deref(&self) -> &Self::Target {
+        self.node.object.downcast_ref().unwrap()
+    }
+}
+
+impl<O: 'static> DerefMut for Context<'_, O> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.node.object.downcast_mut().unwrap()
+    }
+}
+
 pub struct Handle<O> {
     node: Rc<RefCell<Node>>,
     _marker: PhantomData<O>,
 }
 
 impl<O> Handle<O> {
+    pub fn new(object: O) -> Self
+    where
+        O: 'static,
+    {
+        Self {
+            node: Rc::new(RefCell::new(Node {
+                object: Box::new(object),
+                listeners: Vec::new(),
+            })),
+            _marker: PhantomData,
+        }
+    }
+
+    pub fn bind<O2, M>(
+        &self,
+        other: &Handle<O2>,
+        mut listener: impl FnMut(&mut Context<O2>, M) + 'static,
+    ) where
+        O2: 'static,
+        M: Clone + 'static,
+    {
+        let other = other.clone();
+        self.listen::<M>(move |msg| {
+            let mut cx = Context {
+                handle: other.clone(),
+                node: other.node.borrow_mut(),
+            };
+            listener(&mut cx, msg.clone());
+        })
+    }
+
     pub fn listen<M: 'static>(&self, mut listener: impl FnMut(&M) + 'static) {
         let listener = ListenerData {
-            type_id: listener.type_id(),
+            msg_type_id: TypeId::of::<M>(),
+            listener_type_id: listener.type_id(),
             f: Box::new(move |msg| listener(msg.downcast_ref().unwrap())),
         };
         self.node.borrow_mut().listeners.push(listener);
@@ -34,12 +99,19 @@ impl<O> Handle<O> {
         if let Some(idx) = node
             .listeners
             .iter()
-            .position(|listener_data| listener_data.type_id == listener.type_id())
+            .position(|listener_data| listener_data.listener_type_id == listener.type_id())
         {
             node.listeners.remove(idx);
             true
         } else {
             false
+        }
+    }
+
+    pub fn cx(&self) -> Context<O> {
+        Context {
+            handle: self.clone(),
+            node: self.node.borrow_mut(),
         }
     }
 
@@ -62,6 +134,16 @@ impl<O> Handle<O> {
     }
 }
 
+impl<O> Clone for Handle<O> {
+    fn clone(&self) -> Self {
+        Self {
+            node: self.node.clone(),
+            _marker: self._marker.clone(),
+        }
+    }
+}
+
+
 pub struct Listener {
     type_id: TypeId,
     node: Rc<RefCell<Node>>,
@@ -73,7 +155,7 @@ impl Listener {
         if let Some(idx) = node
             .listeners
             .iter()
-            .position(|listener| listener.type_id == self.type_id)
+            .position(|listener| listener.listener_type_id == self.type_id)
         {
             node.listeners.remove(idx);
             true
