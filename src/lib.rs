52a7331b5363080d061c9d2b39abcb5ffae29219
Matt Hunzinger <matthunz2@gmail.com>
2024-01-06 02:08:06 -0500

    Try out new Composable design

--- src/lib.rs
+++ src/lib.rs
@@ -1,113 +1,111 @@
-//! A high-performance runtime for reactive objects.
-//!
-//! Concoct is an event-driven state management system that run anywhere (including `#![no_std]`).
-//!
-//! ## Objects
-//!
-//! The main component is an [`Object`], a reactive group of data,
-//! that is composed of signals and slots.
-//! Signals can be bound to slots to create an efficient reactive graph
-//! where signals send events to slots.
-//!
-//! ## Signals
-//! A signal is an event emitted by an object.
-//! Multiple signals can be added to an object by implementing [`Signal`]
-//! for the specific messages.
-//!
-//! ## Slots
-//! A slot is a callback into the mutable state of an object.
-//! As such, they can be implemented as normal functions that take a
-//! [`Context`] self parameter and a message.
-//!
-//! ```no_run
-//! use concoct::{Context, Object, Signal};
-//!
-//! #[derive(Default)]
-//! struct Counter {
-//!     value: i32,
-//! }
-//!
-//! impl Object for Counter {}
-//!
-//! impl Signal<i32> for Counter {}
-//!
-//! impl Counter {
-//!     fn set_value(cx: &mut Context<Self>, value: i32) {
-//!         if cx.value != value {
-//!             cx.value = value;
-//!             cx.emit(value);
-//!         }
-//!     }
-//! }
-//!
-//! let a = Counter::create();
-//! let b = Counter::create();
-//!
-//! a.bind(&b, Counter::set_value);
-//!
-//! Counter::set_value(&mut a.cx(), 2);
-//!
-//! assert_eq!(a.borrow().value, 2);
-//! assert_eq!(b.borrow().value, 2);
-//! ```
-//!
-//! ## Installation
-//! The easiest way to get started is using the `full` feature flag.
-//! ```ignore
-//! cargo add concoct --features full
-//! ```
-//!
-//! ## Feature flags
-//!  - `full`: Enables all the features below.
-//!  - `channel`: Enables the `channel` module for channels between objects.
-//!
-#![no_std]
-#![deny(missing_docs)]
-#![cfg_attr(docsrs, feature(doc_cfg))]
+use std::{any::Any, marker::PhantomData, sync::Arc};
+use tokio::sync::mpsc;
 
-extern crate alloc;
-
-use alloc::boxed::Box;
-use alloc::rc::Weak;
-use alloc::vec::Vec;
-use core::any::{Any, TypeId};
-use core::cell::RefCell;
-
-mod context;
-pub use self::context::Context;
-
-mod handle;
-pub use self::handle::{Binding, Handle};
-
-mod object;
-pub use self::object::Object;
-
-pub mod signal;
-pub use self::signal::Signal;
-
-#[cfg(feature = "channel")]
-#[cfg_attr(docsrs, doc(cfg(feature = "channel")))]
-pub mod channel;
-
-#[derive(Clone)]
-struct ListenerData {
-    msg_id: TypeId,
-    slot_id: TypeId,
-    node: Weak<RefCell<Node>>,
-    listen: fn(Weak<RefCell<Node>>, usize, &dyn Any),
-    slot: usize,
+pub struct Receiver<M> {
+    rx: mpsc::UnboundedReceiver<Box<dyn Any>>,
+    _marker: PhantomData<M>,
 }
 
-struct Node {
-    object: Box<dyn Any>,
-    listeners: Vec<ListenerData>,
-    bindings: Vec<Binding>,
+impl<M> Receiver<M> {
+    pub async fn recv(&mut self) -> Option<M>
+    where
+        M: 'static,
+    {
+        self.rx.recv().await.map(|any| *any.downcast().unwrap())
+    }
 }
 
-impl Drop for Node {
-    fn drop(&mut self) {
-        for listener in &self.bindings {
-            listener.unbind();
+pub struct Context<M> {
+    mapper: Option<Arc<dyn Fn(M) -> Box<dyn Any>>>,
+    tx: mpsc::UnboundedSender<Box<dyn Any>>,
+}
+
+impl<M> Context<M> {
+    pub fn new() -> (Self, Receiver<M>) {
+        let (tx, rx) = mpsc::unbounded_channel();
+        (
+            Self { mapper: None, tx },
+            Receiver {
+                rx,
+                _marker: PhantomData,
+            },
+        )
+    }
+
+    pub fn send(&self, msg: M)
+    where
+        M: 'static,
+    {
+        let boxed = if let Some(mapper) = self.mapper.as_ref() {
+            mapper(msg)
+        } else {
+            Box::new(msg)
+        };
+        self.tx.send(boxed).unwrap();
+    }
+}
+
+impl<M> Clone for Context<M> {
+    fn clone(&self) -> Self {
+        Self {
+            mapper: self.mapper.clone(),
+            tx: self.tx.clone(),
         }
+    }
+}
+
+pub trait Composable<M> {
+    type State;
+
+    fn compose(&mut self, cx: &mut Context<M>) -> Self::State;
+
+    fn recompose(&mut self, cx: &mut Context<M>, state: &mut Self::State);
+
+    fn map<F, M1>(self, f: F) -> Map<Self, F, M>
+    where
+        Self: Sized,
+        F: Fn(M) -> M1 + 'static,
+        M1: 'static,
+    {
+        Map {
+            view: self,
+            f: Arc::new(f),
+            _marker: PhantomData,
+        }
+    }
+}
+
+pub struct Map<V, F, M> {
+    view: V,
+    f: Arc<F>,
+    _marker: PhantomData<M>,
+}
+
+impl<V, F, M1, M2> Composable<M1> for Map<V, F, M2>
+where
+    V: Composable<M2>,
+    F: Fn(M2) -> M1 + 'static,
+    M1: 'static,
+{
+    type State = V::State;
+
+    fn compose(&mut self, cx: &mut Context<M1>) -> Self::State {
+        let f = self.f.clone();
+        let mapper: Arc<dyn Fn(M2) -> Box<dyn Any>> = Arc::new(move |msg| Box::new((f)(msg)));
+        let mut cx = Context {
+            mapper: Some(mapper),
+            tx: cx.tx.clone(),
+        };
+        self.view.compose(&mut cx)
+    }
+
+    fn recompose(&mut self, cx: &mut Context<M1>, state: &mut Self::State) {
+        let f = self.f.clone();
+        let mapper: Arc<dyn Fn(M2) -> Box<dyn Any>> = Arc::new(move |msg| Box::new((f)(msg)));
+        let mut cx = Context {
+            mapper: Some(mapper),
+            tx: cx.tx.clone(),
+        };
+        self.view.recompose(&mut cx, state)
     }
 }
