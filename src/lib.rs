5babb53bc6d126def4781244e300dcf162f70a78
Matt Hunzinger <matthunz2@gmail.com>
2024-01-09 21:51:17 -0500

    Create new Task trait based on Future

--- src/lib.rs
+++ src/lib.rs
@@ -1,98 +1,32 @@
-use std::sync::Arc;
-use tokio::sync::mpsc;
+use std::{rc::Rc, task::Poll};
 
-pub mod composable;
-pub use composable::Composable;
-
-pub struct Context<M> {
-    send: Arc<dyn Fn(M) + Send + Sync>,
+pub trait Wake {
+    fn wake(self: Rc<Self>);
 }
 
-impl<M> Context<M> {
-    pub fn new(send: Arc<dyn Fn(M) + Send + Sync>) -> Self {
-        Self { send }
-    }
+#[derive(Clone)]
+pub struct Waker {
+    wake: Rc<dyn Wake>,
+}
 
-    pub fn send(&self, msg: M) {
-        (self.send)(msg)
+impl Waker {
+    pub fn wake(self) {
+        self.wake.wake()
     }
 }
 
-impl<M> Clone for Context<M> {
-    fn clone(&self) -> Self {
-        Self {
-            send: self.send.clone(),
-        }
+impl<W: Wake + 'static> From<Rc<W>> for Waker {
+    fn from(value: Rc<W>) -> Self {
+        Self { wake: value }
     }
 }
 
-pub trait Model<M> {
-    fn handle(&mut self, msg: M);
+pub struct Context<'a> {
+    waker: &'a Waker,
 }
 
-pub struct Composer<T, F, S, M> {
-    model: T,
-    composable: F,
-    state: Option<S>,
-    cx: Context<M>,
-    rx: mpsc::UnboundedReceiver<M>,
-}
+pub trait Task {
+    type Output;
 
-impl<T, F, S, M> Composer<T, F, S, M> {
-    pub fn new(model: T, composable: F) -> Self
-    where
-        M: Send + 'static,
-    {
-        let (tx, rx) = mpsc::unbounded_channel();
-        let cx = Context::new(Arc::new(move |msg| {
-            tx.send(msg).unwrap();
-        }));
-        Self {
-            model,
-            composable,
-            state: None,
-            cx,
-            rx,
-        }
-    }
-
-    pub fn compose<C>(&mut self)
-    where
-        T: Model<M>,
-        F: FnMut(&T) -> C,
-        C: Composable<M, State = S>,
-    {
-        let state = (self.composable)(&self.model).compose(&mut self.cx);
-        self.state = Some(state);
-    }
-
-    pub fn recompose<C>(&mut self)
-    where
-        T: Model<M>,
-        F: FnMut(&T) -> C,
-        C: Composable<M, State = S>,
-    {
-        let state = self.state.as_mut().unwrap();
-        (self.composable)(&self.model).recompose(&mut self.cx, state);
-    }
-
-    pub async fn handle(&mut self)
-    where
-        T: Model<M>,
-        M: 'static,
-    {
-        let msg = self.rx.recv().await.unwrap();
-        self.model.handle(msg);
-    }
-
-    pub  fn try_handle(&mut self)
-    where
-        T: Model<M>,
-        M: 'static,
-    {
-        if let Ok(msg) = self.rx.try_recv() {
-            self.model.handle(msg);
-        }
-       
-    }
+    fn poll(&mut self, cx: &mut Context) -> Poll<Self::Output>;
 }
